b644fef9852c571d5cdb4deb0452910c38fb2aa0
crevulus <crevulus@gmail.com>
2020-07-13 16:22:54 +0200

    commenting

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const startPause = document.querySelector("#start-pause");
   const width = 10;
 
-  //tetrominoes
+  // Tetrominoes. Contains an array of arrays. Each inner array points to indexes. indexes combined = shape of tetro.
   const iTetro = [
     [1, width + 1, width * 2 + 1, width * 3 + 1],
     [width, width + 1, width + 2, width + 3],
@@ -46,8 +46,8 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentPosition = 4;
   let currentRotation = 0;
 
+  // Choose a tetro at random and 1st rotation shape
   let random = Math.floor(Math.random() * tetros.length);
-
   let currentTetro = tetros[random][currentRotation];
 
   function draw() {
@@ -62,14 +62,16 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   timerId = setInterval(moveDown, 500);
+
   function moveDown() {
     undraw();
-    currentPosition += width;
+    currentPosition += width; // Adds 10 to index, thus moving it down one line
     draw();
     stopTetro();
   }
 
   function stopTetro() {
+    // If tetro hits a stop point (bottom of grid or other tetro), then stop class is applied
     if (
       currentTetro.some((cell) =>
         squares[currentPosition + cell + width].classList.contains("stop")
@@ -78,17 +80,20 @@ document.addEventListener("DOMContentLoaded", () => {
       currentTetro.forEach((cell) =>
         squares[currentPosition + cell].classList.add("stop")
       );
+      // Triggers next tetro
       random = Math.floor(Math.random() * tetros.length);
       currentTetro = tetros[random][currentRotation];
       currentPosition = 4;
       draw();
     }
   }
+
+  // Key controls
   function control(event) {
     if (event.keyCode === 37) {
       moveLeft();
     } else if (event.keyCode === 38) {
-      // rotate
+      rotate();
     } else if (event.keyCode === 39) {
       moveRight();
     } else if (event.keyCode === 40) {
@@ -100,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function moveLeft() {
     undraw();
     const isAtLeftEdge = currentTetro.some(
-      (cell) => (currentPosition + cell) % width === 0
+      (cell) => (currentPosition + cell) % width === 0 // Because index of divs down left hand side are all factors of 10
     );
     if (!isAtLeftEdge) currentPosition -= 1;
     if (
@@ -115,7 +120,7 @@ document.addEventListener("DOMContentLoaded", () => {
   function moveRight() {
     undraw();
     const isAtRightEdge = currentTetro.some(
-      (cell) => (currentPosition + cell) % width === width - 1
+      (cell) => (currentPosition + cell) % width === width - 1 // Because index of divs down right hand side are all factors of 9
     );
     if (!isAtRightEdge) currentPosition += 1;
     if (
@@ -125,6 +130,17 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       currentPosition -= 1;
     }
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    currentRotation++;
+    // Resets iteration through tetro shapes array
+    if (currentRotation === currentTetro.length) {
+      currentRotation = 0;
+    }
+    currentTetro = tetros[random][currentRotation];
     draw();
   }
 });
